7ac9f6fb5a3363648a2d22288c9ea8b531c0237c
aljudson <alanjudson@gmail.com>
2013-05-29 14:32:30 -0400

    Added good views for survey and customer models

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -8,4 +8,18 @@ class SurveysController < ApplicationController
       @survey = Survey.new
     end
   
+    def create
+    @survey = Survey.new(params[:survey])
+	    if @survey.save
+	      redirect_to surveys_path
+	    else
+	      # This line overrides the default rendering behavior, which
+	      # would have been to render the "create" view.
+	      render :action => "new"
+	    end
+	end
+
+	def show
+		Survey.find(params[:id])
+	end
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ActiveRecord::Base
   attr_accessible :capac_of_fryer, :chicken_fat, :comments_equip, :comments_recupe, :customer_id, :date, :desired_location, :desired_map, :freq_of_disposal, :grease_trap_service, :grill_plate, :install_electicity_outside, :integrated_solution, :kind_of_oil, :location, :location_map, :number_of_fryers, :organic_pu, :outdoor_electricity, :present_equip, :suggested_equip, :user_id
-  belongs_to :user
+  belongs_to :admin
   belongs_to :survey
 end
