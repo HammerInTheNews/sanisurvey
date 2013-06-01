7a9b9c0141ca69d59a864b8c2b361c22191c9aa5
ajudson <alanjudson@gmail.com>
2013-05-31 21:46:26 -0400

    Made restful controller for customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,13 +7,66 @@ class CustomersController < ApplicationController
     #format.xls #{ send_data @customers.to_csv(col_sep: "\t") }
     end  
 	end
+  # GET /customers/1
+  # GET /customers/1.json
+  def show
+    @customer = Customer.find(params[:id])
 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @customer }
+    end
+  end
 	def new
       @customer = customer.new
-    end
+  end
 
-    def import
-      Customer.import(params[:file])
-      redirect_to customers_path, notice: "Customers"
+  def import
+    Customer.import(params[:file])
+    redirect_to customers_path, notice: "Customers"
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+    def create
+    @customer = Customer.new(params[:customer])
+
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
+        format.json { render json: @customer, status: :created, location: @customer }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+    def update
+    @customer = Customer.find(params[:id])
+
+    respond_to do |format|
+      if @customer.update_attributes(params[:customer])
+        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    # DELETE /customers/1
+  # DELETE /customers/1.json
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+
+    respond_to do |format|
+      format.html { redirect_to customers_url }
+      format.json { head :no_content }
+    end
+  end
 end
