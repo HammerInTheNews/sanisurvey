50a998ddeb7586bb43cba8a5936445fcc7d16df1
ajudson <alanjudson@gmail.com>
2013-06-01 21:29:19 -0400

    I added downloading csv functionality

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
     respond_to do |format|
     format.html
     format.csv { send_data @customers.to_csv }
-    #format.xls #{ send_data @customers.to_csv(col_sep: "\t") }
+    format.xls { send_data @customers.to_csv(col_sep: "\t") }
     end  
 	end
   # GET /customers/1
