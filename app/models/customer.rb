4dfb2138a402410d1ecaa392ee78002a4d993366
ajudson <alanjudson@gmail.com>
2013-06-02 21:43:19 -0400

    added survey links to customers, but it only goes to all surveys

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ActiveRecord::Base
   attr_accessible :contact_name, :customer_number, :email, :phone, :postal_code, :province, :restaurant_name, :street_address, :city
   has_many :surveys
 
+  after_create :create_survey
+
   def self.to_csv(options = {})
   	CSV.generate(options) do |csv|
   		csv << column_names
@@ -9,7 +11,16 @@ class Customer < ActiveRecord::Base
   			csv << customer.attributes.values_at(*column_names)
   		end
   	end
-  end			
+  end		
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
+    when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
 
   def self.import(file)
     spreadsheet = open_spreadsheet(file)
@@ -21,13 +32,9 @@ class Customer < ActiveRecord::Base
       customers.save!
     end
   end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-    when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
-    when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
-    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
-    else raise "Unknown file type: #{file.original_filename}"
-    end
+  
+  def create_survey
+    self.surveys.create    
   end
+
 end
