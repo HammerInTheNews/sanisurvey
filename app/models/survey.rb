6a75f2e824e2d30cd5d7bef2b019546525646a51
Alan Judson <hammeringthenews@gmail.com>
2013-06-24 21:21:27 -0400

    added customer number to the field

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   attr_accessible :capac_of_fryer, :chicken_fat, :comments_equip, :comments_recupe, :customer_id, :date, :desired_location, :desired_map, :freq_of_disposal, :grease_trap_service, :grill_plate, :install_electicity_outside, :integrated_solution, :kind_of_oil, :location, :location_map, :number_of_fryers, :organic_pu, :outdoor_electricity, :present_equip, :suggested_equip, :user_id, :image
   
   belongs_to :admin
-  belongs_to :survey
+  belongs_to :customer
   has_attached_file :image, 
     :styles => { :medium => "160x160>", :thumb => "180x180>", :large => "660x660>" }, 
     :default_url => "/images/:style/missing.png",
